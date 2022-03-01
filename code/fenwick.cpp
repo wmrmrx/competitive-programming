3ef8045c7b7b0ccfe94403f8b3cd6db707a72dc6
Willian Mori <wnmrmr@gmail.com>
2022-03-01 18:31:58 -0300

    add zeroindexed fenwick

--- code/fenwick.cpp
+++ code/fenwick.cpp
@@ -1,20 +1,24 @@
-// ONE INDEXED
-template <typename T> struct Fenwick {
-	const size_t size;
+template <typename T, bool OneIndexed = true> struct Fenwick {
 	vector<T> bit;
-	Fenwick(size_t size): size(size) {
-		bit = vector<T>(size+1);
+	Fenwick(size_t size) {
+		bit.assign(size, 0);
 	}
 	void update(size_t id, const T val) {
+		if(!OneIndexed) {
+			id += 1;
+		}
 		while(id <= size) {
-			bit[id] += val;
+			bit[id-1] += val;
 			id += 1ULL<<__builtin_ctzll(id);
 		}
 	}
 	T query(size_t id) {
+		if(!OneIndexed) {
+			id += 1;
+		}
 		T sum = 0; 
 		while(id > 0) {
-			sum += bit[id];
+			sum += bit[id-1];
 			id -= 1ULL<<__builtin_ctzll(id);
 		}
 		return sum;
