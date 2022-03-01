a170ca425b8e831b91246410190e0ba9dde914ef
Willian Mori <wnmrmr@gmail.com>
2022-03-01 17:37:17 -0300

    change types

--- code/compression.cpp
+++ code/compression.cpp
@@ -4,14 +4,14 @@ vector<int> compress(vector<int> &arr) {
 		comp_set.insert(i);
 	}
 	vector<int> ret(comp_set.size());
-	map<int,int> comp_mp;
-	int cnt = 0;
+	map<int,uint32_t> comp_mp;
+	uint32_t cnt = 0;
 	for(int i: comp_set) {
 		comp_mp[i] = cnt;
 		ret[cnt] = i;
 		cnt++;
 	}
-	for(int &x: arr) {
+	for(int& x: arr) {
 		x = comp_mp[x];
 	}
 	return ret;
--- code/fenwick.cpp
+++ code/fenwick.cpp
@@ -1,28 +1,21 @@
-struct Fenwick {
-	int size;
-	vector<int> bit;
-	Fenwick(int _size) {
-		size = _size;
-		bit = vector<int>(size+1);
+// ONE INDEXED
+template <typename T> struct Fenwick {
+	const size_t size;
+	vector<T> bit;
+	Fenwick(size_t size): size(size) {
+		bit = vector<T>(size+1);
 	}
-	Fenwick(int _size, int v[]) {
-		size = _size;
-		bit = vector<int>(size+1);
-		for(int i=1;i<=size;i++) {
-			update(i,v[i]);
-		}
-	}
-	void update(int id, int val) {
+	void update(size_t id, const T val) {
 		while(id <= size) {
 			bit[id] += val;
-			id += id&-id;
+			id += 1ULL<<__builtin_ctzll(id);
 		}
 	}
-	int query(int id) {
-		int sum = 0; 
+	T query(size_t id) {
+		T sum = 0; 
 		while(id > 0) {
 			sum += bit[id];
-			id -= id&-id;
+			id -= 1ULL<<__builtin_ctzll(id);
 		}
 		return sum;
 	}
