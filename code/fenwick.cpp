ff9a6c24ecfe99d3941c2d5fea1b8db08cf51f3d
Willian Mori <wnmrmr@gmail.com>
2022-03-05 09:27:49 -0300

    a

--- code/fenwick.cpp
+++ code/fenwick.cpp
@@ -2,11 +2,11 @@ template <bool OneIndexed = true> struct Fenwick {
 	const size_t size; vector<int64_t> bit;
 	Fenwick(size_t size): size(size), bit(size+1) {}
 	void update(size_t id, const int64_t val) {
-		for(id+=!OneIndexed;id<=size;id+=(1ULL<<__builtin_ctzll(id)))bit[id]+=val;
+		for(id+=!OneIndexed;id<=size;id+=id&-id)bit[id]+=val;
 	}
 	int64_t query(size_t id) {
 		size_t sum=0;
-		for(id+=!OneIndexed;id<=size;id-=(1ULL<<__builtin_ctzll(id)))sum+=bit[id];
+		for(id+=!OneIndexed;id<=size;id-=id&-id)sum+=bit[id];
 		return sum;
 	}
 };
