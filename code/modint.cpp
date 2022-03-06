07dcff9e67fe299116d5ab062ee43b3bed893c88
Willian Mori <wnmrmr@gmail.com>
2022-03-06 20:31:09 -0300

    unchecked

--- code/modint.cpp
+++ code/modint.cpp
@@ -1,10 +1,13 @@
-// constexpr int64_t MOD = 1000000007;
+//constexpr int64_t MOD = 1000000007;
 struct Z {
 	int64_t x;
-	Z(const int64_t x=0): x(x%MOD<0?x%MOD+MOD:x%MOD) {}
+	Z(const int64_t _x=0, const bool unchecked=false) {
+		if(unchecked) x = _x;
+		else{int64_t m=_x%MOD;if(m<0)x=m+MOD;else x=m;}
+	}
 	Z fexp(uint64_t e) const {
-		Z ret(1);
-		for(Z copy(x);e>0;e/=2,copy*=copy)if(e&1)ret*=copy;
+		Z ret(1,true);
+		for(Z copy(x,true);e>0;e/=2,copy*=copy)if(e&1)ret*=copy;
 		return ret;
 	}
 	Z inv()const{return this->fexp(MOD-2);}
@@ -12,8 +15,8 @@ struct Z {
 	void operator-=(const Z rhs){x-=rhs.x;if(x<0)x+=MOD;}
 	void operator*=(const Z rhs){x*=rhs.x;x%=MOD;}
 	void operator/=(const Z rhs){x*=rhs.inv().x;x%=MOD;}
-	Z operator+(const Z rhs)const{Z ret(x);ret+=rhs;return ret;}
-	Z operator-(const Z rhs)const{Z ret(x);ret-=rhs;return ret;}
-	Z operator*(const Z rhs)const{Z ret(x);ret*=rhs;return ret;}
-	Z operator/(const Z rhs)const{Z ret(x);ret/=rhs;return ret;}
+	Z operator+(const Z rhs)const{Z ret(x,true);ret+=rhs;return ret;}
+	Z operator-(const Z rhs)const{Z ret(x,true);ret-=rhs;return ret;}
+	Z operator*(const Z rhs)const{Z ret(x,true);ret*=rhs;return ret;}
+	Z operator/(const Z rhs)const{Z ret(x,true);ret/=rhs;return ret;}
 };
