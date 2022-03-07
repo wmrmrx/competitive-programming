6c23acbc3b0e1c759442b94cabab3b13dedea41b
Willian Mori <wnmrmr@gmail.com>
2022-03-07 11:50:42 -0300

    copy

--- code/modint.cpp
+++ code/modint.cpp
@@ -4,7 +4,7 @@ struct Z {
 	Z(const int64_t x=0): x(x%MOD<0?x%MOD+MOD:x%MOD) {}
 	Z fexp(uint64_t e) const {
 		Z ret{1};
-		for(Z copy{x};e>0;e/=2,copy*=copy)if(e&1)ret*=copy;
+		for(Z copy=*this;e>0;e/=2,copy*=copy)if(e&1)ret*=copy;
 		return ret;
 	}
 	Z inv()const{return this->fexp(MOD-2);}
@@ -13,8 +13,8 @@ struct Z {
 	void operator*=(const Z rhs){x*=rhs.x;x%=MOD;}
 	void operator/=(const Z rhs){x*=rhs.inv().x;x%=MOD;}
 	bool operator==(const Z rhs)const{return x==rhs.x;}
-	Z operator+(const Z rhs)const{Z ret{x};ret+=rhs;return ret;}
-	Z operator-(const Z rhs)const{Z ret{x};ret-=rhs;return ret;}
-	Z operator*(const Z rhs)const{Z ret{x};ret*=rhs;return ret;}
-	Z operator/(const Z rhs)const{Z ret{x};ret/=rhs;return ret;}
+	Z operator+(const Z rhs)const{Z ret=*this;ret+=rhs;return ret;}
+	Z operator-(const Z rhs)const{Z ret=*this;ret-=rhs;return ret;}
+	Z operator*(const Z rhs)const{Z ret=*this;ret*=rhs;return ret;}
+	Z operator/(const Z rhs)const{Z ret=*this;ret/=rhs;return ret;}
 };
