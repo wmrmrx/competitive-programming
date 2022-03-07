2a9eeceb8372670eeba1e3b8d6c53ba34eeb3529
Willian Mori <wnmrmr@gmail.com>
2022-03-07 19:06:31 -0300

    seg

--- code/seg.cpp
+++ code/seg.cpp
@@ -24,19 +24,19 @@ struct Seg {
 	}
 	void build(Node& cur, size_t cl, size_t cr) {
 		if(cl == cr) { 
-			return Data(); 
+			cur.data = Data();
+			return; 
 		}
 		size_t mid = (cl+cr)/2;
 		Node& p1 = nodes[cur.lchild = new_node()];
 		Node& p2 = nodes[cur.rchild = new_node()];
-		build(p1, cl, mid, v);
-		build(p2, mid+1, cr, v);
+		build(p1, cl, mid);
+		build(p2, mid+1, cr);
 		cur.data = p1.data.merge(p2.data);
 	}
 	Data query(const size_t l, const size_t r, Node& cur, size_t cl, size_t cr) {
 		if(r < cl || cr < l) {
-			Data empty();
-			return empty;
+			return Data();
 		}
 		if(l <= cl && cr <= r) return cur.data;
 		size_t mid = (cl+cr)/2;
@@ -44,38 +44,25 @@ struct Seg {
 		Data ret2 = query(l,r,nodes[cur.rchild],mid+1,cr);
 		return ret1.merge(ret2);
 	}
-	int64_t lower_bound(int64_t val, Node& cur, size_t cl, size_t cr) {
-		if(cl == cr) return cl;
-		size_t mid = (cl+cr)/2;
-		Node& p1 = nodes[cur.lchild];
-		Node& p2 = nodes[cur.rchild];
-		if(p1.data.x < val) return lower_bound(val-p1.data.x,p2,mid+1,cr);
-		else return lower_bound(val,p1,cl,mid);
-	}
-	void update(const size_t pos, const int64_t val, Node& cur, size_t cl, size_t cr) {
+	void update(const size_t pos, const Data data, Node& cur, size_t cl, size_t cr) {
 		if(pos < cl || cr < pos) return;
 		if(cl == cr) { 
-			cur.data.x += val; 
+			cur.data.x += data.x;
 			return; 
 		}
 		size_t mid = (cl+cr)/2;
-		update(pos, val, nodes[cur.lchild], cl, mid);
-		update(pos, val, nodes[cur.rchild], mid+1, cr);
+		Node& p1 = nodes[cur.lchild];
+		Node& p2 = nodes[cur.rchild];
+		update(pos, data, p1, cl, mid);
+		update(pos, data, p2, mid+1, cr);
 		cur.data = p1.data.merge(p2.data);
 	}
-	int64_t query(size_t l, size_t r) {
+	Data query(size_t l, size_t r) {
 		Node& cur = nodes[0];
-		return query(l,r,cur,0,size-1).x;
+		return query(l,r,cur,0,size-1);
 	}
-	size_t lower_bound(size_t l, size_t r, int64_t val) {
+	void update(size_t pos, Data data) {
 		Node& cur = nodes[0];
-		int64_t q = query(l,r);
-		if(q < val) return -1;
-		val += query(0, r) - q;
-		return lower_bound(val,cur,0,size-1);
-	}
-	void update(size_t pos, int64_t val) {
-		Node& cur = nodes[0];
-		update(pos,val,cur,0,size-1);
+		update(pos,data,cur,0,size-1);
 	}
 };
