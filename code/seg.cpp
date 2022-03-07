54544cd5a5626c5bf541d37d3f06b0d5589cdd02
Willian Mori <wnmrmr@gmail.com>
2022-03-07 08:48:31 -0300

    seg

--- code/seg.cpp
+++ code/seg.cpp
@@ -1,8 +1,15 @@
 struct Seg {
+	struct Data {
+		int64_t x;
+		Data(const int64_t x=0): x(x) {}
+		Data merge(const Data& rhs) const { 
+			return Data(x+rhs.x);
+		}
+	};
 	struct Node {
 		size_t lchild, rchild;
-		int64_t data;
-		Node(): lchild(0), rchild(0), data(0) {}
+		Data data;
+		Node(): lchild(0), rchild(0), data() {}
 	};
 	const size_t size;
 	vector<Node> nodes;
@@ -16,64 +23,60 @@ struct Seg {
 		return nodes.size()-1;
 	}
 	void build(Node& cur, size_t cl, size_t cr) {
-		if(cl == cr) {
-			return;
+		if(cl == cr) { 
+			cur.data = Data(); 
+			return; 
 		}
 		size_t mid = (cl+cr)/2;
 		Node& p1 = nodes[cur.lchild = new_node()];
 		Node& p2 = nodes[cur.rchild = new_node()];
 		build(p1, cl, mid, v);
 		build(p2, mid+1, cr, v);
-		cur.data = p1.data + p2.data;
+		cur.data = p1.data.merge(p2.data);
 	}
-	int64_t query(size_t l, size_t r, Node& cur, size_t cl, size_t cr) {
+	Data query(size_t l, size_t r, Node& cur, size_t cl, size_t cr) {
 		if(r < cl || cr < l) {
-			return 0;
+			Data empty();
+			return empty;
 		}
-		if(l <= cl && cr <= r) {
-			return cur.data;
-		}
+		if(l <= cl && cr <= r) return cur.data;
 		size_t mid = (cl+cr)/2;
 		Node& p1 = nodes[cur.lchild];
 		Node& p2 = nodes[cur.rchild];
 		auto ret1 = query(l,r,p1,cl,mid);
 		auto ret2 = query(l,r,p2,mid+1,cr);
-		return ret1 + ret2;
+		cur.data = ret1.merge(ret2);
 	}
-	size_t lower_bound(int64_t val, Node& cur, size_t cl, size_t cr) {
-		if(cl == cr) {
-			return cl;
-		}
+	int64_t lower_bound(int64_t val, Node& cur, size_t cl, size_t cr) {
+		if(cl == cr) return cl;
 		size_t mid = (cl+cr)/2;
 		Node& p1 = nodes[cur.lchild];
 		Node& p2 = nodes[cur.rchild];
-		if(p1.data < val) {
-			return lower_bound(val - p1.data, p2, mid+1, cr);
-		} else {
-			return lower_bound(val, p1, cl, mid);
-		}
+		if(p1.data.x < val) return lower_bound(val-p1.data.x,p2,mid+1,cr);
+		else return lower_bound(val,p1,cl,mid);
 	}
 	void update(size_t pos, int64_t val, Node& cur, size_t cl, size_t cr) {
-		if(pos < cl || cr < pos) {
-			return;
-		}
-		if(cl == cr) {
-			cur.data += val;
-			return;
+		if(pos < cl || cr < pos) return;
+		if(cl == cr) { 
+			cur.data.x += val; 
+			return; 
 		}
 		size_t mid = (cl+cr)/2;
 		Node& p1 = nodes[cur.lchild];
 		Node& p2 = nodes[cur.rchild];
 		update(pos, val, p1, cl, mid);
 		update(pos, val, p2, mid+1, cr);
-		cur.data = p1.data + p2.data;
+		cur.data = p1.data.merge(p2.data);
 	}
 	int64_t query(size_t l, size_t r) {
 		Node& cur = nodes[0];
-		return query(l,r,cur,0,size-1);
+		return query(l,r,cur,0,size-1).x;
 	}
-	size_t lower_bound(int64_t val) {
+	size_t lower_bound(size_t l, size_t r, int64_t val) {
 		Node& cur = nodes[0];
+		int64_t q = query(l,r);
+		if(q < val) return -1;
+		val += query(0, r) - q;
 		return lower_bound(val,cur,0,size-1);
 	}
 	void update(size_t pos, int64_t val) {
