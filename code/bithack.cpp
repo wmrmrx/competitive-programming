e1b19a5f5742be7c5d9ebf379c2b19b8ddd2e7e1
wnmrmr <wnmrmr@gmail.com>
2022-05-01 18:59:49 -0300

    bithack

--- code/bithack.cpp
+++ code/bithack.cpp
@@ -1,12 +1,15 @@
 // iterator through all masks with n bits and m set bits
+// use: for(auto mask: BitIterator(n,m) { ... }
 struct BitIterator {
+	struct Mask {
+		uint32_t mask;
+		Mask(uint32_t mask): mask(mask) {}
+		bool operator!=(const Mask& rhs) const { return mask < rhs.mask; };
+		void operator++(){const uint32_t t=mask|(mask-1);mask=(t+1)|(((~t&-~t)-1)>>__builtin_ffs(mask));}
+		uint32_t operator*() const { return mask; }
+	};
 	const uint32_t n, m;
-	uint32_t cur;
 	BitIterator(const uint32_t n, const uint32_t m): n(n), m(m) {}
-	BitIterator(const uint32_t n, const uint32_t m, const uint32_t cur): n(n), m(m), cur(cur) {}
-	void operator++(){const uint32_t t=cur|(cur-1);cur=(t+1)|(((~t&-~t)-1)>>__builtin_ffs(cur));}
-	uint32_t operator*() const { return cur; }
-	bool operator!=(const BitIterator& other) const { return cur < other.cur; };
-	BitIterator begin() const { return BitIterator(n, m, (1<<m)-1); }
-	BitIterator end() const { return BitIterator(n, m, (1<<n)); }
+	Mask begin() const { return Mask((1<<m)-1); }
+	Mask end() const { return Mask((1<<n)); }
 };
