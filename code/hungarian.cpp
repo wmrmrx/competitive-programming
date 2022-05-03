11a64b502cb460bb21d701792a6a2060e2832685
wnmrmr <wnmrmr@gmail.com>
2022-05-03 16:33:56 -0300

    hungarian

--- code/hungarian.cpp
+++ code/hungarian.cpp
@@ -1,14 +1,13 @@
-// IF MINIMIZATION SET EDGES -WEIGHT, RETURN -RESULT;
-bool zero(double x) { return fabs(x) < 1e-9; }
-struct Hungarian {
-	consteval size_t NAN = 1ULL<<60;
+constexpr size_t INVALID = 1ULL<<60;
+bool zero(double x) { return fabs(x) <= 1e-9; }
+template <bool MAXIMIZE=false> struct Hungarian {
 	vector<vector<double>> w;
 	vector<size_t> ml, mr; // ml: matched vertexes of left side
-	Hungarian(size_t n): w(n, vector<size_t>(n)), ml(n), mr(n) {}
-	void set(double weight, size_t i, size_t j) { w[i][j] = weight; //-weight }
+	Hungarian(size_t n): w(n, vector<double>(n)), ml(n), mr(n) {}
+	void set(size_t i, size_t j, double weight) { w[i][j] = MAXIMIZE?-weight:weight; }
 	double assign() {
-		fill(ml.begin(), ml.end(), NAN);
-		fill(mr.begin(), mr.end(), NAN);
+		fill(ml.begin(), ml.end(), INVALID);
+		fill(mr.begin(), mr.end(), INVALID);
 		uint32_t match = 0; // number of matchings
 		size_t n = w.size();
 		vector<double> d(n), y(n), z(n); // d: Delta
@@ -19,23 +18,24 @@ struct Hungarian {
 			for(size_t i=1;i<n;i++) z[j]=min(z[j], w[i][j]-y[i]);
 		}
 		for(size_t i=0;i<n;i++) for(size_t j=0;j<n;j++) {
-			if(mr[j] == NAN && zero(w[i][j]-y[i]-z[j])) {
+			if(mr[j] == INVALID && zero(w[i][j]-y[i]-z[j])) {
 				ml[i] = j; mr[j] = i; match++; break;
 			}
 		}
 		for(;match<n;match++) {
 			size_t s = 0, i, j;
-			while(ml[s] != NAN) s++;
-			fill(dad.begin(), dad.end(), NAN);
+			while(ml[s] != INVALID) s++;
+			fill(dad.begin(), dad.end(), INVALID);
 			fill(vis.begin(), vis.end(), 0);
 			for(size_t k=0;k<n;k++) d[k]=w[s][k]-y[s]-z[k];
 			while(true) {
-				size_t j = NAN;
+				j = INVALID;
 				for(size_t k=0;k<n;k++) 
-					if(!vis[k] && (j == NAN || d[k] < d[j])) 
+					if(!vis[k] && (j == INVALID || d[k] < d[j])) 
 						j = k; 
+				assert(j != INVALID);
 				vis[j] = 1; i = mr[j];
-				if(i == NAN) break;
+				if(i == INVALID) break;
 				for(size_t k=0;k<n;k++) if(!vis[k]) {
 					double new_d = d[j] + w[i][k] - y[i] - z[k];
 					if(d[k] > new_d) d[k]=new_d, dad[k]=j;
@@ -43,11 +43,11 @@ struct Hungarian {
 			}
 			for(size_t k=0;k<n;k++) if(k != j && vis[k]) {
 				double diff = d[k] - d[j];
-				z[k] += w;
-				y[mr[k]] -= w;
+				z[k] += diff;
+				y[mr[k]] -= diff;
 			}
 			y[s] += d[j];
-			while(dad[j] != NAN) {
+			while(dad[j] != INVALID) {
 				ml[mr[j] = mr[dad[j]]] = j;
 				j = dad[j];
 			}
@@ -55,6 +55,6 @@ struct Hungarian {
 		}
 		double ret = 0;
 		for(size_t i=0;i<n;i++) ret += w[i][ml[i]];
-		return ret; //-ret
+		return MAXIMIZE?-ret:ret;
 	}
-}
+};
