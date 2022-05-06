90dcac765bb88b8d982ee7d89350bf0f9b69ac33
wnmrmr <wnmrmr@gmail.com>
2022-05-06 10:32:35 -0300

    hun

--- code/hungarian.cpp
+++ code/hungarian.cpp
@@ -1,4 +1,4 @@
-constexpr size_t NONE = 1ULL<<60;
+constexpr size_t NONE = numeric_limits<size_t>::max();
 bool zero(double x) { return fabs(x) <= 1e-9; }
 template <bool MAXIMIZE=false> struct Hungarian {
 	vector<vector<double>> w;
@@ -8,17 +8,11 @@ template <bool MAXIMIZE=false> struct Hungarian {
 	double assign() {
 		fill(ml.begin(), ml.end(), NONE);
 		fill(mr.begin(), mr.end(), NONE);
-		uint32_t match = 0; // number of matchings
 		size_t n = w.size();
 		vector<double> y(n), z(n), d(n);
 		vector<size_t> dad(n); 
 		vector<bool> S(n), T(n);
 		for(size_t i=0;i<n;i++) y[i] = *max_element(w[i].begin(), w[i].end());
-		for(size_t i=0;i<n;i++) for(size_t j=0;j<n;j++) {
-			if(mr[j] == NONE && zero(w[i][j]-y[i]-z[j])) {
-				ml[i] = j; mr[j] = i; match++; break;
-			}
-		}
 		auto update_dual = [&](double delta) {
 			for(size_t i=0;i<n;i++) if(S[i]) y[i] -= delta;
 			for(size_t j=0;j<n;j++) {
@@ -26,42 +20,43 @@ template <bool MAXIMIZE=false> struct Hungarian {
 				else d[j] -= delta;
 			}
 		};
-		for(;match<n;match++) {
-			fill(dad.begin(), dad.end(), NONE);
+		size_t i=0, j;
+		for(uint64_t match=0;match<n;match++) {
+			while(ml[i] != NONE) i++;
 			fill(S.begin(), S.end(), 0); fill(T.begin(), T.end(), 0);
-			size_t i=0, j;
-			while(ml[i] != NONE) i++; S[i] = true;
+			S[i] = true;
 			for(size_t t=0;t<n;t++) d[t] = y[i]+z[t]-w[i][t];
+			fill(dad.begin(), dad.end(), NONE);
 			while(true) {
-				j = NONE;
-				for(size_t t=0;t<n;t++) if(!T[t] && (j==NONE||d[t]<d[j])) j=t;
+				j = min_element(d.begin(), d.end()) - d.begin();
 				if(!zero(d[j])) {
-					update_dual(); 
+					update_dual(d[j]); 
 					continue;
 				}
-				size_t s = mr[j];
-				if(s == NONE) {
-					for(s=0;s<n;s++) if(S[s]) {
-						if(zero(y[s]+z[j]-w[s][j])) break;
-					}
+				if(mr[j] == NONE) {
+					size_t s=0;
+					while(!S[s] || !zero(y[s]+z[j]-w[s][j])) s++;
 					dad[j] = ml[s];
+					while(dad[j] != NONE) {
+						mr[j] = mr[dad[j]];
+						ml[mr[j]] = j;
+						j = dad[j];
+					}
+					ml[i] = j; mr[j] = i;
 					break;
-				}
-				S[s] = true; T[j] = true;
-				for(size_t t=0;t<n;t++) if(!T[t]) {
-					double val = y[s]+z[t]-w[s][t];
-					if(val < d[t]) d[t] = val, dad[t] = j;
+				} else {
+					size_t _i = mr[j];
+					S[_i] = true; T[j] = true;
+					d[j] = numeric_limits<double>::max();
+					for(size_t t=0;t<n;t++) if(!T[t]) {
+						double val = y[_i]+z[t]-w[_i][t];
+						if(val < d[t]) d[t] = val, dad[t] = j;
+					}
 				}
 			}
-			while(dad[j] != NONE) {
-				mr[j] = mr[dad[j]];
-				ml[mr[j]] = j;
-				j = dad[j];
-			}
-			ml[i] = j; mr[j] = i;
 		}
 		double ret = 0;
-		for(size_t i=0;i<n;i++) ret += w[i][ml[i]];
+		for(i=0;i<n;i++) ret += w[i][ml[i]];
 		return MAXIMIZE?ret:-ret;
 	}
 };
