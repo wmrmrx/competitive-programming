2b3220be33102e8eb2c8c4babb6002cbd63bb6b1
wnmrmr <wnmrmr@gmail.com>
2022-05-06 23:39:49 -0300

    fix

--- code/hungarian.cpp
+++ code/hungarian.cpp
@@ -1,21 +1,21 @@
 constexpr size_t NONE = numeric_limits<size_t>::max();
-bool zero(double x) { return fabs(x) < 1e-9; }
+bool zero(int64_t x) { return x == 0; }
 template <bool MAXIMIZE=false> struct Hungarian {
-	vector<vector<double>> w;
+	vector<vector<int64_t>> w;
 	vector<size_t> ml, mr; // ml: matched vertexes of left side
-	vector<double> y, z, d;
+	vector<int64_t> y, z, d;
 	vector<bool> S, T;
-	Hungarian(size_t n): w(n,vector<double>(n)),ml(n),mr(n),y(n),z(n),d(n),S(n),T(n) {}
-	void set(size_t i, size_t j, double weight) { w[i][j] = MAXIMIZE?weight:-weight; }
-	double assign() {
+	Hungarian(size_t n): w(n,vector<int64_t>(n)),ml(n),mr(n),y(n),z(n),d(n),S(n),T(n) {}
+	void set(size_t i, size_t j, int64_t weight) { w[i][j] = MAXIMIZE?weight:-weight; }
+	int64_t assign() {
 		size_t n = w.size();
 		ml.assign(n, 0); mr.assign(n, 0);
 		for(size_t i=0;i<n;i++) y[i] = *max_element(w[i].begin(), w[i].end());
 		z.assign(n, 0);
-		const auto kuhn = [&](size_t s, const auto&& self) -> bool {
+		auto kuhn = [&](size_t s, auto&& self) -> bool {
 			if(S[s]) return false; S[s] = 1;
 			for(size_t t=0;t<n;t++) {
-				double diff = y[s]+z[t]-w[s][t];
+				int64_t diff = y[s]+z[t]-w[s][t];
 				if(T[t]) continue;
 				if(zero(diff)) {
 					T[t] = 1;
@@ -28,11 +28,11 @@ template <bool MAXIMIZE=false> struct Hungarian {
 			return false;
 		};
 		for(size_t i=0;i<n;i++) {
-			d.assign(numeric_limits<double>::max, n);
+			d.assign(numeric_limits<int64_t>::max(), n);
 			while(true) {
 				S.assign(0, n); T.assign(0, n);
 				if(kuhn(i,kuhn)) break;
-				double delta = numeric_limits<double>::max();
+				int64_t delta = numeric_limits<int64_t>::max();
 				for(size_t j=0;j<n;j++) if(!T[j]) delta=min(delta, d[j]);
 				for(size_t s=0;s<n;s++) if(S[s]) y[s] -= delta;
 				for(size_t j=0;j<n;j++) {
@@ -41,7 +41,7 @@ template <bool MAXIMIZE=false> struct Hungarian {
 				}
 			}
 		}
-		double ret = 0;
+		int64_t ret = 0;
 		for(size_t i=0;i<n;i++) ret += y[i];
 		for(size_t j=0;j<n;j++) ret += z[j];
 		return MAXIMIZE?ret:-ret;
