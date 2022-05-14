3025d3adc9f91f14f166401696ff60fd024bf074
wnmrmr <wnmrmr@gmail.com>
2022-05-14 19:56:00 -0300

    no

--- code/fenwick.cpp
+++ code/fenwick.cpp
@@ -1,11 +1,11 @@
-template <typename T, bool OneIndexed = true> struct Fenwick {
-	const size_t size; vector<T> bit;
+template <bool OneIndexed = true> struct Fenwick {
+	const size_t size; vector<int64_t> bit;
 	Fenwick(size_t size): size(size), bit(size+1) {}
-	void update(size_t id, const T val) {
+	void update(size_t id, const int64_t val) {
 		for(id+=!OneIndexed;id<=size;id+=id&-id)bit[id]+=val;
 	}
-	T query(size_t id) {
-		T sum=0;
+	int64_t query(size_t id) {
+		int64_t sum=0;
 		for(id+=!OneIndexed;id>0;id-=id&-id)sum+=bit[id];
 		return sum;
 	}
