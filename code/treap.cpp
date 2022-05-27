c9620013f18ed815f72dc5f436763ebba97d3f41
wmrmrx <wmrmrx+github@gmail.com>
2022-05-27 19:27:32 -0300

    add treap

--- code/treap.cpp
+++ code/treap.cpp
@@ -1,77 +1,83 @@
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+template <typename T>
 struct Treap {
-	struct Data {
-		int64_t x;
-		Data(const int64_t x=0): x(x) {}
-		Data merge(const Data& rhs) const { 
-			return Data(x+rhs.x);
+	struct node {
+		int cnt, h;
+		T data;
+		array<node*,2> ch;
+
+		node(T data=0): cnt(1), h(rng()), data(data), ch({0, 0}) {}
+
+		node* refresh() {
+			cnt = 1;
+			for(int side: {0, 1}) 
+				cnt += ch[side] ? ch[side]->cnt : 0;
+			return this;
 		}
 	};
-	struct Node {
-		size_t lchild, rchild;
-		uint32_t sub;
-		const uint32_t h;
-		Data data;
-		Node(uint32_t h): lchild(0), rchild(0), sub(1), h(h), data() {}
-	};
-	size_t root;
-	vector<uint32_t> h;
-	vector<Node> nodes;
-	size_t new_node() {
-		nodes.push_back(Node(h[nodes.size()]));
-		return nodes.size()-1;
+
+	int sz;
+	node *root;
+	unique_ptr<node[]> arena;
+
+	Treap(int prealloc): sz(0), root(0), arena(new node[prealloc]) {}
+
+	node* new_node(T data) {
+		arena[sz] = node(data);
+		return &arena[sz++];
 	}
-	vector<uint32_t> random_permutation(const size_t size) const {
-		vector<uint32_t> ret(size);
-		iota(ret.begin(),ret.end(),0);
-		mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-		shuffle(ret.begin(), ret.end(), rng);
-		return ret;
-	}
-	Treap(const size_t size=0): root(0), h(random_permutation(size)) {
-		nodes.reserve(size+1);
-		new_node();
-	}
-	size_t cnt_left(const Node& t) const { return t.lchild==0?0:(size_t)nodes[t.lchild].sub; }
-	size_t cnt_right(const Node& t) const { return t.rchild==0?0:(size_t)nodes[t.rchild].sub; }
-	void refresh(const Node& t) { t.sub=1+cnt_left(t)+cnt_right(t); }
-	void split(const size_t pos, size_t idx, size_t cur, size_t& tl, size_t& tr) {
-		if(cur == 0) { tl = tr = 0; return; }
-		size_t cur_idx = idx + cnt_left(nodes[cur]);
-		if(pos < cur_idx) tr=cur, split(pos,idx,nodes[cur].lchild,tl,nodes[cur].lchild);
-		else tl=cur, split(pos,cur_idx+1,nodes[cur].rchild,nodes[cur].rchild,tr);
-		refresh(cur);
-	}
-	void merge(size_t tl, size_t tr, size_t& dest) {
-		if(tl==0||tr==0) { dest = tl+tr; return; }
-		if(nodes[tl].h < nodes[tr].h) dest=tr, merge(tl,nodes[tr].lchild,nodes[tr].lchild);
-		else dest=tl, merge(nodes[tl].rchild,tr,nodes[tl].rchild);
-		refresh(dest);
-	}
-	int64_t& find(size_t index) {
-		if(root == 0 || nodes[root].sub >= index) return nullptr;
-		size_t cur_idx = cnt_left(nodes[cur]);
-		size_t cur = root;
-		while(cur_index != index) {
-			if(index < cur_index) {
-				cur_idx-=cnt_left(nodes[cur]);
-				cur=nodes[cur].lchild;
-				cur_idx+=cnt_left(nodes[cur]);
-			} else {
-				cur_idx++;
-				cur=nodes[cur].rchild;
-			}
+
+	node* merge(node *l, node *r) {
+		if(!l || !r) return l ? l : r;
+		if(l->h < r->h) {
+			r->ch[0] = merge(l, r->ch[0]);
+			return r->refresh();
 		}
-		return nodes[cur].data.x;
+		else {
+			l->ch[1] = merge(l->ch[1], r);
+			return l->refresh();
+		}
 	}
-	// TODO
-	void insert(size_t index) {
 
+	array<node*, 2> split(node* a, int pos, int rank = 0) {
+		if(!a) return {0, 0};
+		int cur_rank = rank + (a->ch[0] ? a->ch[0]->cnt : 0);
+		if(pos <= cur_rank) {
+			auto res = split(a->ch[0], pos, rank);
+			a->ch[0] = res[1];
+			return { res[0], a->refresh() };
+		} else {
+			auto res = split(a->ch[1], pos, cur_rank+1);
+			a->ch[1] = res[0];
+			return { a->refresh(), res[1] };
+		}
 	}
-	void erase(size_t index) {
 
+	void insert(T data, int idx) {
+		if(!root) root = new_node(data);
+		else {
+			node* n = new_node(data);
+			auto s = split(root, idx);
+			s[0] = merge(s[0], n);
+			root = merge(s[0], s[1]);
+		}
+	}
+
+	void erase(int idx) {
+		auto s1 = split(root, idx);
+		auto s2 = split(s1[1], 1);
+		root = merge(s1[0], s2[1]);
+	}
+
+	T& get(int idx) {
+		auto* cur = root;
+		int rank = 0;
+		int cur_rank = rank + (cur->ch[0] ? cur->ch[0]->cnt : 0);
+		while(cur_rank != idx) {
+			if(idx < cur_rank) cur = cur->ch[0];
+			else cur = cur->ch[1], rank = cur_rank+1;
+			cur_rank = rank + (cur->ch[0] ? cur->ch[0]->cnt : 0);
+		}
+		return cur->data;
 	}
 };
-
-
-
-
