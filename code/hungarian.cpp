8d6737b604b86bcbf39a1cf73a8aceaf8df2ad9a
wmrmrx <wmrmrx+github@gmail.com>
2022-05-28 11:08:55 -0300

    refactor hungarian

--- code/hungarian.cpp
+++ code/hungarian.cpp
@@ -3,29 +3,30 @@ constexpr double INIT_W=-1e100;
 constexpr int NONE = numeric_limits<int>::max();
 bool zero(double x) { return abs(x) < 1e-9; }
 template <bool MAXIMIZE> struct Hungarian {
-	vector<unique_ptr<double[]>> w;
+	int n, m;
+	unique_ptr<unique_ptr<double[]>[]> w;
 	unique_ptr<int[]> ml, mr; // ml: matched vertexes of left side
 	unique_ptr<double[]> y, z, d;
 	unique_ptr<bool[]> S, T;
 
-	Hungarian(int n): ml(new int[n]), mr(new int[n]),
-	y(new double[n]), z(new double[n]), d(new double[n]),
-	S(new bool[n]), T(new bool[n]) {
-		w.reserve(n);
+	Hungarian(int n, int m): n(n), m(m), 
+	ml(new int[n]), mr(new int[m]),
+	y(new double[n]), z(new double[m]), d(new double[m]),
+	S(new bool[n]), T(new bool[m]) {
+		w.reset(new unique_ptr<double[]>[n]);
 		for(int i=0;i<n;i++) {
-			w.push_back(unique_ptr<double[]>(new double[n]));
-			fill(w[i].get(), w[i].get()+n, INIT_W);
+			w[i].reset(new double[m]);
+			fill(w[i].get(), w[i].get()+m, INIT_W);
 		}
 	}
 
 	void set(int i, int j, double weight) { w[i][j] = MAXIMIZE?weight:-weight; }
 
 	double assign() {
-		int n = w.size();
-		fill(ml.get(), ml.get()+n, NONE); fill(mr.get(), mr.get()+n, NONE);
-		for(int i=0;i<n;i++) y[i] = *max_element(w[i].get(), w[i].get()+n);
-		fill(z.get(), z.get()+n, 0);
-		for(int i=0;i<n;i++) for(int j=0;j<n;j++) {
+		fill(ml.get(), ml.get()+n, NONE); fill(mr.get(), mr.get()+m, NONE);
+		for(int i=0;i<n;i++) y[i] = *max_element(w[i].get(), w[i].get()+m);
+		fill(z.get(), z.get()+m, 0);
+		for(int i=0;i<n;i++) for(int j=0;j<m;j++) {
 			if(mr[j] == NONE && zero(y[i]+z[j]-w[i][j])) {
 				ml[i] = j; mr[j] = i;
 				break;
@@ -33,7 +34,7 @@ template <bool MAXIMIZE> struct Hungarian {
 		}
 		auto kuhn = [&](int s, auto&& self) -> bool {
 			if(S[s]) return false; S[s] = 1;
-			for(int t=0;t<n;t++) {
+			for(int t=0;t<m;t++) {
 				double diff = y[s]+z[t]-w[s][t];
 				if(T[t]) continue;
 				if(zero(diff)) {
@@ -48,21 +49,22 @@ template <bool MAXIMIZE> struct Hungarian {
 		};
 		for(int i=0;i<n;i++) {
 			if(ml[i] != NONE) continue;
-			fill(d.get(), d.get()+n, numeric_limits<double>::max());
+			fill(d.get(), d.get()+m, numeric_limits<double>::max());
 			while(true) {
-				fill(S.get(), S.get()+n, false); fill(T.get(), T.get()+n, false);
+				fill(S.get(), S.get()+n, false); fill(T.get(), T.get()+m, false);
 				if(kuhn(i,kuhn)) break;
 				double delta = numeric_limits<double>::max();
-				for(int j=0;j<n;j++) if(!T[j]) delta=min(delta, d[j]);
+				for(int j=0;j<m;j++) if(!T[j]) delta=min(delta, d[j]);
 				for(int s=0;s<n;s++) if(S[s]) y[s] -= delta;
-				for(int j=0;j<n;j++) {
+				for(int j=0;j<m;j++) {
 					if(T[j]) z[j] += delta;
 					else d[j] -= delta;
 				}
 			}
 		}
-		double ret = 0;
-		for(int k=0;k<n;k++) ret += y[k] + z[k];
-		return MAXIMIZE?ret:-ret;
+		double res = 0;
+		for(int i=0;i<n;i++) res += y[i];
+		for(int j=0;j<m;j++) res += z[j];
+		return MAXIMIZE?res:-res;
 	}
 };
