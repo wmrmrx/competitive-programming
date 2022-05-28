9d15210dd21c7299f8c266f465bb28d70d8e20cc
wmrmrx <wmrmrx+github@gmail.com>
2022-05-27 22:32:53 -0300

    improve treap

--- code/treap.cpp
+++ code/treap.cpp
@@ -1,17 +1,20 @@
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-template <typename T>
+template <typename DATA>
 struct Treap {
 	struct node {
 		int cnt, h;
-		T data;
+		DATA data;
+		node* dad;
 		array<node*,2> ch;
 
-		node(T data=0): cnt(1), h(rng()), data(data), ch({0, 0}) {}
+		node(DATA data): cnt(1), h(rng()), data(data), dad(0), ch({0, 0}) {}
 
 		node* refresh() {
 			cnt = 1;
-			for(int side: {0, 1}) 
-				cnt += ch[side] ? ch[side]->cnt : 0;
+			for(int side: {0, 1}) if(ch[side]) {
+				ch[side]->dad = this;
+				cnt += ch[side]->cnt;
+			}
 			return this;
 		}
 	};
@@ -22,10 +25,12 @@ struct Treap {
 
 	Treap(int prealloc): sz(0), root(0), arena(new node[prealloc]) {}
 
-	node* new_node(T data) {
+	node* new_node(DATA data) {
 		arena[sz] = node(data);
 		return &arena[sz++];
 	}
+
+	int cnt(node* x) { return x ? x->cnt : 0; }
 
 	node* merge(node *l, node *r) {
 		if(!l || !r) return l ? l : r;
@@ -41,7 +46,7 @@ struct Treap {
 
 	array<node*, 2> split(node* a, int pos, int rank = 0) {
 		if(!a) return {0, 0};
-		int cur_rank = rank + (a->ch[0] ? a->ch[0]->cnt : 0);
+		int cur_rank = rank + cnt(a->ch[0]);
 		if(pos <= cur_rank) {
 			auto res = split(a->ch[0], pos, rank);
 			a->ch[0] = res[1];
@@ -53,14 +58,10 @@ struct Treap {
 		}
 	}
 
-	void insert(T data, int idx) {
-		if(!root) root = new_node(data);
-		else {
-			node* n = new_node(data);
-			auto s = split(root, idx);
-			s[0] = merge(s[0], n);
-			root = merge(s[0], s[1]);
-		}
+	void insert(DATA data, int idx) {
+		auto s = split(root, idx);
+		s[0] = merge(s[0], new_node(data));
+		root = merge(s[0], s[1]);
 	}
 
 	void erase(int idx) {
@@ -69,15 +70,24 @@ struct Treap {
 		root = merge(s1[0], s2[1]);
 	}
 
-	T& get(int idx) {
-		auto* cur = root;
+	int rank(node* cur) {
+		int res;
+		for(res = cnt(cur->ch[0]); cur != root; cur=cur->dad) {
+			node* l = cur->dad->ch[0];
+			if(l != cur) res += 1 + cnt(l);
+		}
+		return res;
+	}
+
+	node* get(int idx) {
+		node* cur = root;
 		int rank = 0;
-		int cur_rank = rank + (cur->ch[0] ? cur->ch[0]->cnt : 0);
+		int cur_rank = rank + cnt(cur->ch[0]);
 		while(cur_rank != idx) {
 			if(idx < cur_rank) cur = cur->ch[0];
 			else cur = cur->ch[1], rank = cur_rank+1;
-			cur_rank = rank + (cur->ch[0] ? cur->ch[0]->cnt : 0);
+			cur_rank = rank + cnt(cur->ch[0]);
 		}
-		return cur->data;
+		return cur;
 	}
 };
