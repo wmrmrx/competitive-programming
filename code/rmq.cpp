cd944b58ef12107eae09f522b17fa469cf6b643f
wmrmrx <wmrmrx+github@gmail.com>
2022-05-28 11:54:16 -0300

    refactor rmq

--- code/rmq.cpp
+++ code/rmq.cpp
@@ -1,18 +1,16 @@
-template <typename T, bool MaxQuery = false> struct RMQ {
-	const vector<vector<T>> dp;
-	T min_or_max(const T a, const T b) const { return MaxQuery ? max<T>(a,b) : min<T>(a,b); }
-	const vector<vector<T>> build(const size_t size, const T v[]) const {
-		const size_t log = 64-__builtin_clzll(size);
-		vector<vector<T>> ret(log, vector<T>(size));
-		for(size_t i=0;i<size;i++)ret[0][i]=v[i];
-		for(size_t l=1;l<log;l++) for(size_t i=0;i<size;i++) 
-			ret[l][i] = min_or_max(ret[l-1][i],ret[l-1][min(i+(1<<(l-1)), size)]);
-		return ret;
+template <typename T, bool MAX = false> struct RMQ {
+	vector<vector<T>> dp;
+	T ops(T a, T b) { return MAX ? max<T>(a,b) : min<T>(a,b); }
+	RMQ(int sz, T v[]) {
+		int log = 64-__builtin_clzll(sz);
+		dp.assign(log, vector<T>(sz));
+		for(int i=0;i<sz;i++) dp[0][i] = v[i];
+		for(int l=1;l<log;l++) for(int i=0;i<sz;i++) 
+			dp[l][i] = ops(dp[l-1][i],dp[l-1][min(i+(1<<(l-1)), sz-1)]);
 	}
-	RMQ(const size_t size, const T v[]): dp(build(size,v)) {}
-	T query(size_t a, size_t b) const {
+	T query(int a, int b)  {
 		if(a == b) return dp[0][a];
-		size_t pot = 63-__builtin_clzll(b-a);
+		int pot = 63-__builtin_clzll(b-a);
 		return min_or_max(dp[pot][a], dp[pot][b-(1<<pot)+1]);
 	}
 };
