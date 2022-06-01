2b5a79d4d0219a35f15da7223a9546159edb4bf8
Willian Mori <wnmrmr@gmail.com>
2022-06-01 10:09:46 -0300

    rewrite matrix

--- code/matrix.cpp
+++ code/matrix.cpp
@@ -1,86 +1,30 @@
-template <typename T> struct Matrix {
-	const size_t lin, col;
-	vector<vector<T>> m;
-	Matrix(size_t lin, size_t col): lin(lin), col(col) {
-		m.assign(lin,vector<T>(col));
+template <typename T> 
+struct Matrix {
+	int lin, col;
+	unique_ptr<unique_ptr<T[]>[]> m;
+
+	Matrix(int lin, int col): lin(lin), col(col), m(new unique_ptr<T[]>[lin]) {
+		for(int i=0;i<lin;i++) m[i].reset(new T[col]);
+		for(int i=0;i<lin;i++) fill_n(m[i].get(), col, 0);
 	}
-	Matrix operator*(Matrix& B) const {
-		assert(col == B.lin);
-		size_t ret_lin = lin, ret_col = B.col;
-		Matrix ret(ret_lin, ret_col);
-		for(size_t i=0;i<ret_lin;i++) {
-			for(size_t k=0;k<col;k++) {
-				for(size_t j=0;j<ret_col;j++) {
-					ret.m[i][j] += m[i][k]*B.m[k][j];
-				}
-			}
-		}
-		return ret;
+
+	Matrix operator*(Matrix& rhs) const {
+		assert(col == rhs.lin);
+		Matrix res(lin, rhs.col);
+		for(int i=0;i<lin;i++) 
+			for(int k=0;k<col;k++)
+				for(int j=0;j<rhs.col;j++)
+					res.m[i][j] += m[i][k]*rhs.m[k][j];
+		return res;
 	}
-	void operator*=(Matrix& B) {
-		assert(col == B.lin);
-		size_t ret_lin = lin, ret_col = B.col;
-		Matrix ret(ret_lin, ret_col);
-		for(size_t i=0;i<ret_lin;i++) {
-			for(size_t k=0;k<col;k++) {
-				for(size_t j=0;j<ret_col;j++) {
-					ret.m[i][j] += m[i][k]*B.m[k][j];
-				}
-			}
-		}
-		lin = ret.lin;
-		col = ret.col;
-		m = ret.m;
-	}
+
 	Matrix exp(uint64_t num) const {
 		assert(lin == col);
-		size_t N = lin;
-		Matrix I(N,N);
-		Matrix copy = *this;
-		for(size_t i=0;i<N;i++) {
-			I.m[i][i] = 1;
-		}
-		while(num) {
-			if(num&1) {
-				I *= copy;
-			}
-			num >>= 1;
-			copy *= copy;
-		}
-		return I;
-	}
-	//Matrix operator__OPERATOR__(const Matrix& B) {
-	//	assert(lin == B.lin && col == B.col);
-	//	Matrix ret(lin, col);
-	//	for(size_t i=0;i<lin;i++) {
-	//		for(size_t j=0;j<col;j++) {
-	//			ret.m[i][j] = m[i][j] __OPERATOR__ B.m[i][j]; 
-	//		}
-	//	}
-	//	return ret;
-	//}
-	//void operator__OPERATOR__=(const Matrix& B) {
-	//	assert(lin == B.lin && col == B.col);
-	//	Matrix ret(lin, col);
-	//	for(size_t i=0;i<lin;i++) {
-	//		for(size_t j=0;j<col;j++) {
-	//			ret.m[i][j] = m[i][j] __OPERATOR__ B.m[i][j]; 
-	//		}
-	//	}
-	//	lin = ret.lin;
-	//	col = ret.col;
-	//	m = ret.m;
-	//}
-	void display() {
-		for(size_t i=0;i<lin;i++) {
-			for(size_t j=0;j<col;j++) {
-				cout << m[i][j] << ' ';
-			}
-			if(i == lin-1) {
-				cout << endl;
-			} else {
-				cout << '\n';
-			}
-		}
+		int n = lin;
+		Matrix res(n,n);
+		for(int i=0;i<n;i++) res.m[i][i] = 1;
+		for(Matrix copy=*this;num;num/=2,copy=copy*copy)
+			if(num&1) res = res*copy;
+		return res;
 	}
 };
