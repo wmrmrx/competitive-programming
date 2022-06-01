66a9d918c73c2a6f631028e97489a8eba9ef0257
Willian Mori <wnmrmr@gmail.com>
2022-06-01 10:14:25 -0300

    rewrite point

--- code/point.cpp
+++ code/point.cpp
@@ -1,26 +1,22 @@
+template <typename T>
 struct Point {
-	int64_t x,y;
-	Point(int64_t x, int64_t y): x(x), y(y) {}
-	int64_t dot(Point &rhs) const {
+	T x,y;
+
+	Point(T x, T y): x(x), y(y) {}
+
+	T dot(Point &rhs) {
 		return x*rhs.x + y*rhs.y;
 	}
-	int64_t cross(Point& rhs) const {
+
+	T cross(Point& rhs) {
 		return x*rhs.y - y*rhs.x;
 	}
-	Point operator+(Point& rhs) const {
-		return Point(x+rhs.x,y+rhs.y);
-	}
-	Point operator-(Point& rhs) const {
-		return Point(x-rhs.x,y-rhs.y);
-	}
-	bool operator==(Point& rhs) const {
-		return x == rhs.x && y == rhs.y;
-	}
-	bool operator<(Point& rhs) const {
-		if( x != rhs.x ) {
-			return x < rhs.x;
-		} else {
-			return y < rhs.y;
-		}
+
+	Point operator+(Point& rhs) { return Point(x+rhs.x,y+rhs.y); }
+	Point operator-(Point& rhs) { return Point(x-rhs.x,y-rhs.y); }
+	bool operator==(Point& rhs) { return x == rhs.x && y == rhs.y; }
+	bool operator<(Point& rhs) {
+		if( x != rhs.x ) return x < rhs.x;
+		else return y < rhs.y;
 	}
 };
