c3a8760b1ba10e467825bd1b6ea95d17e11e11e6
wmrmrx <wmrmrx+github@gmail.com>
2022-06-09 22:12:31 -0300

    remove alph

--- code/suffix_array.cpp
+++ code/suffix_array.cpp
@@ -6,8 +6,7 @@ struct SuffixArray {
 		iota(p.begin()+1, p.end(), 0);
 		sort(p.begin()+1, p.end(), [&](int i, int j) { return s[i] < s[j]; });
 		p[0] = sz++;
-		int alph = *max_element(s, s+sz) - *min_element(s, s+sz) + 1;
-		vector<int> ra(sz), newra(sz), newp(sz), cnt(2+max(sz, alph));
+		vector<int> ra(sz), newra(sz), newp(sz), cnt(sz+2);
 		ra[p[1]] = 1;
 		for(int i=2;i<sz;i++) ra[p[i]] = ra[p[i-1]] + (s[p[i]] != s[p[i-1]]);
 		for(int k=1;k<sz;k*=2) {
