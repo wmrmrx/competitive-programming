8715ac5107dfaba860a8bb7396e05811a10d4e04
wmrmrx <wmrmrx+github@gmail.com>
2022-06-12 17:27:15 -0300

    rename variables

--- code/dinic.cpp
+++ code/dinic.cpp
@@ -1,49 +1,48 @@
 constexpr int INF = numeric_limits<int>::max();
 struct Dinic {
 	struct edge {
-		int to;
-		int cap, flow;
+		int to, cap, flow;
 	};
 
 	vector<vector<int>> g;
-	vector<int> level;
+	vector<int> lvl;
 	vector<edge> e;
 
-	Dinic(int sz): g(sz), level(sz) {}
+	Dinic(int sz): g(sz), lvl(sz) {}
 
-	void add_edge(int s, int t, int capacity) {
+	void add_edge(int s, int t, int cap) {
 		int id = e.size();
 		g[s].push_back(id);
-		e.push_back({t, capacity, 0});
+		e.push_back({t, cap, 0});
 		g[t].push_back(++id);
-		e.push_back({s, capacity, capacity});
+		e.push_back({s, cap, cap});
 	}
 
-	bool BFS(int source, int sink) {
-		fill(level.begin(), level.end(), INF);
-		level[source] = 0;
-		queue<int> q{{source}};
-		while(!q.empty() && level[sink] == INF) {
+	bool BFS(int s, int t) {
+		fill(lvl.begin(), lvl.end(), INF);
+		lvl[s] = 0;
+		queue<int> q{{s}};
+		while(!q.empty() && lvl[t] == INF) {
 			int cur = q.front();
 			q.pop();
 			for(int id: g[cur]) {
 				int prox = e[id].to;
-				if(level[prox] != INF || e[id].cap == e[id].flow)
+				if(lvl[prox] != INF || e[id].cap == e[id].flow)
 					continue;
-				level[prox] = level[cur] + 1;
+				lvl[prox] = lvl[cur] + 1;
 				q.push(prox);
 			}
 		}
-		return level[sink] != INF;
+		return lvl[t] != INF;
 	}
 
-	int DFS(int v, int pool, int start[], int sink) {
-		if(pool == 0) return 0;
-		if(v == sink) return pool;
+	int DFS(int v, int pool, int start[], int t) {
+		if(!pool) return 0;
+		if(v == t) return pool;
 		for(;start[v]<(int)g[v].size();start[v]++) {
 			int id = g[v][start[v]], prox = e[id].to;
-			if(level[v]+1 != level[prox] || e[id].cap == e[id].flow) continue;
-			int pushed = DFS(prox,min(e[id].cap-e[id].flow,pool),start,sink);
+			if(lvl[v]+1 != lvl[prox] || e[id].cap == e[id].flow) continue;
+			int pushed = DFS(prox,min(e[id].cap-e[id].flow,pool),start,t);
 			if(pushed) {
 				e[id].flow += pushed;
 				e[id^1].flow -= pushed;
@@ -53,12 +52,12 @@ struct Dinic {
 		return 0;
 	}
 
-	int max_flow(int source, int sink) {
+	int max_flow(int s, int t) {
 		int total_flow = 0;
 		vector<int> start(g.size());
-		while(BFS(source,sink)) {
+		while(BFS(s,t)) {
 			fill(start.begin(), start.end(), 0);
-			while(int pushed = DFS(source,INF,start.data(),sink)) 
+			while(int pushed = DFS(s,INF,start.data(),t)) 
 				total_flow += pushed;
 		}
 		//reset to initial state
