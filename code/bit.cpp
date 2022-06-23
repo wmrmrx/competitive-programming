e27b52482dbc71a45f1194de85685564fcb94119
wnmrmr <wnmrmr@gmail.com>
2022-06-23 10:06:35 -0300

    fix bit

--- code/bit.cpp
+++ code/bit.cpp
@@ -1,13 +1,10 @@
 template <typename T>
 struct BIT {
 	vector<T> bit;
-
-	BIT(int sz): sz(sz), bit(sz+1) {}
-
+	BIT(int sz): bit(sz+1) {}
 	void update(int id, T val) {
 		for(;id<(int)bit.size();id+=id&-id) bit[id]+=val;
 	}
-
 	T query(int id) {
 		T sum = 0;
 		for(;id>0;id-=id&-id) sum+=bit[id];
