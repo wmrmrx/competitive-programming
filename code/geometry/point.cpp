e70cafe02bbc230079901b6cfd604ba45b4a95a1
wnmrmr <wnmrmr@gmail.com>
2022-07-01 14:59:57 -0300

    updat point

--- code/geometry/point.cpp
+++ code/geometry/point.cpp
@@ -8,4 +8,8 @@ struct point {
 	point operator-(point &rhs) { return {x+rhs.x,y+rhs.y}; }
 	i64 operator*(point &rhs) { return x*rhs.x + y*rhs.y; }
 	i64 operator^(point &rhs) { return x*rhs.y - y*rhs.x; }
+	bool operator<(const point& rhs) const {
+		using tup = tuple<i64, i64>;
+		return tup{x,y} < tup{rhs.x, rhs.y};
+	}
 };
