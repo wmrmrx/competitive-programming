8f4979b2a97f775b5c18355131dc6680ee7c4051
wmrmrx <wmrmrx+github@gmail.com>
2022-07-02 21:36:54 -0300

    fix

--- code/geometry/point.cpp
+++ code/geometry/point.cpp
@@ -3,7 +3,7 @@ using i64 = int64_t;
 struct point {
 	i64 x, y;
 
-	point(): x(0), y(0) {}
+	point(i64 x=0, i64 y=0): x(x), y(y) {}
 
 	point operator+(point rhs) { return {x+rhs.x,y+rhs.y}; }
 	point operator-(point rhs) { return {x-rhs.x,y-rhs.y}; }
