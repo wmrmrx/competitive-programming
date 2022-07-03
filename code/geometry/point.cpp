47a778a6e5fac945a59bb80f607621f591a94e6d
wmrmrx <wmrmrx+github@gmail.com>
2022-07-03 13:14:43 -0300

    refactor

--- code/geometry/point.cpp
+++ code/geometry/point.cpp
@@ -1,6 +1,7 @@
 using i64 = int64_t;
 
 struct point {
+	using tup = tuple<i64, i64>;
 	i64 x, y;
 
 	point(i64 x=0, i64 y=0): x(x), y(y) {}
@@ -9,13 +10,11 @@ struct point {
 	point operator-(point rhs) { return {x-rhs.x,y-rhs.y}; }
 	i64 operator*(point rhs) { return x*rhs.x+y*rhs.y; }
 	i64 operator^(point rhs) { return x*rhs.y-y*rhs.x; }
-	bool operator<(const point& rhs) const {
-		using tup = tuple<i64, i64>;
-		return tup{x,y} < tup{rhs.x, rhs.y};
-	}
+	bool operator<(const point& rhs) const { return tup{x,y} < tup{rhs.x, rhs.y}; }
 };
 
 // angular comparison in [0, 2pi)
+// CORNER CASE: a, b == (0, 0)
 bool ang_cmp(point a, point b) {
 	auto quad = [](point p) -> bool {
 		return p.x < 0 || (p.x == 0 && p.y > 0);
@@ -38,14 +37,27 @@ i64 area2(point a, point b, point c) {
 	return (b-a)^(c-a);
 }
 
+// CORNER CASE: two points are equal
 bool left(point a, point b, point c) {
 	return area2(a,b,c) > 0;
 }
 
+// CORNER CASE: two points are equal
 bool right(point a, point b, point c) {
 	return area2(a,b,c) < 0;
 }
 
 bool collinear(point a, point b, point c) {
 	return area2(a,b,c) == 0;
+}
+
+// Returns 0 if vectors a and b are not parallel.
+// If they are parallel, returns 1 if they have the same direction 
+// and returns -1 otherwise
+// CORNER CASE: a, b == (0, 0)
+int paral(point a, point b) { 
+    if((a ^ b) != 0) return 0;
+    if((a.x > 0) == (b.x > 0) && (a.y > 0) == (b.y > 0))
+        return 1;
+    return -1;
 }
