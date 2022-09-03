477c44b4d6081d17e39fef17c46b49a5a635e766
wmrmrx <wmrmrx@gmail.com>
2022-09-02 23:35:22 -0300

    upd

--- code/geometry/double.cpp
+++ code/geometry/double.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct point {
 	double x, y;
 	
@@ -7,6 +10,12 @@ struct point {
 	point operator-(point rhs) { return point(x-rhs.x, y-rhs.y); }
 	point operator*(double k) { return point(k*x, k*y); }
 	double operator*(point rhs) { return x*rhs.x + y*rhs.y; }
+
+	using tup = tuple<double, double>;
+
+	bool operator<(const point& rhs) const {
+		return tup{x, y} < tup{rhs.x, rhs.y};
+	}
 };
 
 struct segment {
@@ -19,4 +28,11 @@ point proj(segment r, point p) {
 	p = p - a;
 	point v = b-a;
 	return a + v*((p*v)/(v*v));
+}
+
+signed main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+
+	point p = proj(segment(point(0,2), point(1,2)), point(1512,1));
+	cout << p.x << ' ' << p.y << endl;
 }
