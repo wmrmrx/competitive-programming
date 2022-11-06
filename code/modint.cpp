42bbe91a893a72f1c5ac62f4b1cf6b7e7a5ae127
wmrmrx <wmrmrx@gmail.com>
2022-11-06 14:21:26 -0300

    silence warnings

--- code/modint.cpp
+++ code/modint.cpp
@@ -1,7 +1,7 @@
 //constexpr int64_t MOD = 1000000007;
 struct Z {
 	int64_t x;
-	Z(const int64_t x=0): x(x%MOD<0?x%MOD+MOD:x%MOD) {}
+	Z(const int64_t _x=0): x(_x%MOD<0?_x%MOD+MOD:_x%MOD) {}
 	Z fexp(uint64_t e) const {
 		Z ret{1};
 		for(Z copy=*this;e>0;e/=2,copy*=copy)if(e&1)ret*=copy;
