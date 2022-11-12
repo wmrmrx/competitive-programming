55972f0bd2de640ecd1d5b253e16858af8af3ac5
wmrmrx <wmrmrx@gmail.com>
2022-11-12 15:46:28 -0300

    rust

--- rust/template.rs
+++ rust/template.rs
@@ -1,25 +1,99 @@
-struct Scanner {
-    buffer: std::vec::IntoIter<String>
-}
-
-impl Scanner {
-    fn new() -> Scanner {
-        use std::io::Read;
-        let mut s = String::new();
-        let _ = std::io::stdin().read_to_string(&mut s);
-        Scanner {
-            buffer: s.trim().split_whitespace().map(|s| s.to_owned()).collect::<Vec<String>>().into_iter()
-        }
-    }
-    fn next<T: std::str::FromStr<Err = impl std::fmt::Debug>>(&mut self) -> T {
-        return self.buffer.next().unwrap().parse::<T>().unwrap();
-    }
-}
-
-fn solve(scan: &mut Scanner) {
+fn solve(sc: &mut In, bf: &mut Out) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
 }
 
 fn main() {
-    let mut scan = Scanner::new();
-    solve(&mut scan);
+    let mut sc = Scanner::new(std::io::stdin().lock());
+    let mut out = std::io::BufWriter::new(std::io::stdout().lock());
+    solve(&mut sc, &mut out).unwrap();
+    out.flush().unwrap();
+}
+
+////////////////////////
+// BEGIN DEFAULT CODE //
+////////////////////////
+
+#[allow(unused_imports)]
+use std::{
+    collections::{BTreeMap as Map, BTreeSet as Set},
+    io::Write,
+};
+#[allow(unused_imports)]
+use util::{i, int, Scanner};
+type In<'a> = Scanner<'a>;
+type Out<'a> = std::io::BufWriter<std::io::StdoutLock<'a>>;
+/* END DEFAULT IMPORTS */
+
+#[allow(dead_code)]
+mod util {
+    static mut INPUT: Option<String> = None;
+
+    pub struct Scanner<'a> {
+        lock: std::io::StdinLock<'a>,
+        buffer: std::vec::IntoIter<&'a str>,
+    }
+
+    impl<'a> Scanner<'a> {
+        pub fn new(mut lock: std::io::StdinLock<'a>) -> Self {
+            use std::io::BufRead;
+            let mut s = String::new();
+            lock.read_line(&mut s).unwrap();
+            unsafe {
+                INPUT = Some(s);
+            }
+            Scanner {
+                lock,
+                buffer: unsafe {
+                    INPUT
+                        .as_ref()
+                        .unwrap()
+                        .trim()
+                        .split_whitespace()
+                        .collect::<Vec<&str>>()
+                        .into_iter()
+                },
+            }
+        }
+
+        pub fn read<T: std::str::FromStr<Err = impl std::fmt::Debug>>(&mut self) -> T {
+            use std::io::BufRead;
+            loop {
+                if let Some(s) = self.buffer.next() {
+                    return s.parse::<T>().unwrap();
+                } else {
+                    unsafe {
+                        self.lock.read_line(INPUT.as_mut().unwrap()).unwrap();
+                        self.buffer = INPUT
+                            .as_ref()
+                            .unwrap()
+                            .trim()
+                            .split_whitespace()
+                            .collect::<Vec<&str>>()
+                            .into_iter()
+                    }
+                }
+            }
+        }
+
+        pub fn read_vec<T: std::str::FromStr<Err = impl std::fmt::Debug>>(
+            &mut self,
+            size: usize,
+        ) -> Vec<T> {
+            return (0..size).map(|_| self.read()).collect();
+        }
+    }
+
+    pub fn i<S: std::convert::TryInto<usize>>(i: S) -> usize
+    where
+        <S as std::convert::TryInto<usize>>::Error: std::fmt::Debug,
+    {
+        return i.try_into().unwrap();
+    }
+
+    pub fn int<S: std::convert::TryInto<i64>>(i: S) -> i64
+    where
+        <S as std::convert::TryInto<i64>>::Error: std::fmt::Debug,
+    {
+        return i.try_into().unwrap();
+    }
 }
