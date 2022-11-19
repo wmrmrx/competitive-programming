37a7b254cfeac150ca0df69627441e8b4d8cffe1
wmrmrx <wmrmrx@gmail.com>
2022-11-19 15:50:42 -0300

    copy in fenwick

--- rust/fenwick.rs
+++ rust/fenwick.rs
@@ -3,16 +3,14 @@ mod fenwick {
     #[derive(Clone, Debug)]
     pub struct Fenwick<T>
     where
-        T: Default + std::clone::Clone,
-        for<'a> &'a T: std::ops::Add<Output = T>,
+        T: Default + std::clone::Clone + std::marker::Copy + std::ops::Add<Output = T>,
     {
         bit: Vec<T>,
     }
 
     impl<T> Fenwick<T>
     where
-        T: Default + std::clone::Clone,
-        for<'a> &'a T: std::ops::Add<Output = T>,
+        T: Default + std::clone::Clone + std::marker::Copy + std::ops::Add<Output = T>,
     {
         pub fn new(size: usize) -> Self {
             Self {
@@ -20,10 +18,10 @@ mod fenwick {
             }
         }
 
-        pub fn update(&mut self, mut id: usize, val: &T) {
+        pub fn update(&mut self, mut id: usize, val: T) {
             id += 1;
             while id <= self.bit.len() {
-                self.bit[id - 1] = &self.bit[id - 1] + val;
+                self.bit[id - 1] = self.bit[id - 1] + val;
                 id += 1 << id.trailing_zeros();
             }
         }
@@ -32,7 +30,7 @@ mod fenwick {
             let mut res = T::default();
             id += 1;
             while id > 0 {
-                res = &res + &self.bit[id - 1];
+                res = res + self.bit[id - 1];
                 id -= 1 << id.trailing_zeros();
             }
             res
@@ -41,12 +39,15 @@ mod fenwick {
 
     impl<T> Fenwick<T>
     where
-        T: Default + std::clone::Clone,
-        for<'a> &'a T: std::ops::Add<Output = T> + std::ops::Sub<Output = T>,
+        T: Default
+            + std::clone::Clone
+            + std::marker::Copy
+            + std::ops::Add<Output = T>
+            + std::ops::Sub<Output = T>,
     {
         pub fn range(&self, l: usize, r: usize) -> T {
-            &self.query(r)
-                - &if l == 0 {
+            self.query(r)
+                - if l == 0 {
                     T::default()
                 } else {
                     self.query(l - 1)
