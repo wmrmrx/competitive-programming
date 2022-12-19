98500d3d5f48ea723e94929122172b5bd04b0f32
wmrmrx <wmrmrx@gmail.com>
2022-12-19 09:21:15 -0300

    matrix rust

---
+++ rust/matrix.rs
@@ -0,0 +1,31 @@
+#[allow(dead_code)]
+mod matrix {
+    pub struct Matrix<T: Clone + Default> {
+        col: usize,
+        mat: Box<[T]>,
+    }
+
+    impl<T: Clone + Default> Matrix<T> {
+        pub fn new(lin: usize, col: usize) -> Self {
+            Self {
+                col,
+                mat: vec![T::default(); lin * col].into_boxed_slice(),
+            }
+        }
+    }
+
+    impl<T: Clone + Default> std::ops::Index<usize> for Matrix<T> {
+        type Output = [T];
+        fn index(&self, index: usize) -> &Self::Output {
+            &self.mat[index * self.col..][..self.col]
+        }
+    }
+
+    impl<T: Clone + Default> std::ops::IndexMut<usize> for Matrix<T> {
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+            &mut self.mat[index * self.col..][..self.col]
+        }
+    }
+}
+
+use matrix::Matrix;
