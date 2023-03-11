b6bd6ecf3f91c05c36ef6e8af64ea0b9f542fdf7
wmrmrx <wmrmrx@gmail.com>
2023-03-11 16:36:06 -0300

    seg

--- code/seg.cpp
+++ code/seg.cpp
@@ -1,9 +1,9 @@
 struct Data {
-	using B = int;
+	using T = int;
 	int x;
 
-	Data(): x(numeric_limits<int>::max()) {}
-	Data(B _x): x(_x) {}
+	Data(): x(numeric_limits<T>::max()) {}
+	Data(T _x): x(_x) {}
 
 	friend Data operator+(const Data lhs, const Data rhs) {
 		Data res;
@@ -12,69 +12,59 @@ struct Data {
 	}
 };
 
-template <typename D, bool SUBSTITUTION=false, typename U=typename D::B> 
+template <typename D, typename T=typename D::T> 
 class Seg {
 private: 
-	struct node {
-		node *l, *r;
-		D data;
-		node(node* _l=0, node *_r=0, D _data=D()): l(_l), r(_r), data(_data) {}
-	};
-
 	int sz;
-	vector<node> arena;
+	vector<D> data;
 
-	void build(node& cur, const int cl, const int cr, const U* v) {
-		if(cl == cr) {
-			if(v) cur.data = D(v[cl]);
+	void build(int nid, const int l, const int r, const T* v) {
+		if(l == r) {
+			if(v) data[nid] = D(v[l]);
 			return;
 		}
-		const int m = (cl + cr)/2;
-		arena.push_back(node()); cur.l = &arena.back();
-		build(*cur.l, cl, m, v);
-		arena.push_back(node()); cur.r = &arena.back();
-		build(*cur.r, m+1, cr, v);
-		cur.data = cur.l->data + cur.r->data;
+		const int m = (l + r)/2;
+		build(nid + 1, l, m, v);
+		build(nid + 2*(m - l + 1), m+1, r, v);
+		data[nid] = data[nid + 1] + data[nid + 2*(m - l + 1)];
 	}
 
-	D query(const node& cur, const int cl, const int cr, const int ql, const int qr) const {
-		if(qr < cl || cr < ql) return D();
-		if(ql <= cl && cr <= qr) { return cur.data; };
-		const int m = (cl + cr)/2;
-		return query(*cur.l, cl, m, ql, qr) + query(*cur.r, m+1, cr, ql, qr);
+	D query(int nid, const int l, const int r, const int ql, const int qr) const {
+		if(qr < l || r < ql) return D();
+		if(ql <= l && r <= qr) { return data[nid]; };
+		const int m = (l + r)/2;
+		return query(nid + 1, l, m, ql, qr) + query(nid + 2*(m - l + 1), m+1, r, ql, qr);
 	}
 
-	void update(node& cur, const int cl, const int cr, const int pos, const U& val) {
-		if(cr < pos || pos < cl) return;
-		if(cl == cr) { 
-			if constexpr(SUBSTITUTION) cur.data = D(val); 
-			else cur.data = cur.data + D(val); 
+	void update(int nid, const int l, const int r, const int pos, const T& val) {
+		if(r < pos || pos < l) return;
+		if(l == r) { 
+			data[nid] = D(val); 
 			return; 
 		}
-		const int m = (cl + cr)/2;
-		update(*cur.l, cl, m, pos, val); update(*cur.r, m+1, cr, pos, val);
-		cur.data = cur.l->data + cur.r->data;
+		const int m = (l + r)/2;
+		update(nid + 1, l, m, pos, val); 
+		update(nid + 2*(m - l + 1), m+1, r, pos, val);
+		data[nid] = data[nid + 1] + data[nid + 2*(m - l + 1)];
 	}
 public:
-	Seg(const vector<U>& v): sz(v.size()) {
-		arena.reserve(2*sz-1);
-		arena.push_back(node());
-		build(arena[0], 0, sz-1, v.data());
+	Seg(const vector<T>& v): sz(v.size()) {
+		data.resize(2*sz - 1);
+		build(0, 0, sz-1, v.data());
 	}
 
 	Seg(int _sz): sz(_sz) {
-		arena.reserve(2*sz-1);
-		arena.push_back(node());
-		build(arena[0], 0, sz-1, nullptr);
+		data.resize(2*sz - 1);
+		build(0, 0, sz-1, nullptr);
 	}
 
 	D query(const int ql, const int qr) const {
 		assert(0 <= ql && ql <= qr && qr < sz);
-		return query(arena[0], 0, sz-1, ql, qr);
+		return query(0, 0, sz-1, ql, qr);
 	}
 
-	void update(const int pos, const U& val) {
+	void update(const int pos, const T& val) {
 		assert(0 <= pos && pos < sz);
-		update(arena[0], 0, sz-1, pos, val);
+		update(0, 0, sz-1, pos, val);
 	}
 };
