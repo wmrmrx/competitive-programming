64481bd29571e9bb3170798cba15408c7a06e1b0
wmrmrx <wmrmrx@gmail.com>
2023-03-20 22:12:57 -0300

    upd hld

--- code/hld.cpp
+++ code/hld.cpp
@@ -1,10 +1,10 @@
 template<typename SEG, bool EDGE> struct HLD {
 	int n;
-	vector<int> anc, dep, pos, ini, id;
+	vector<int> anc, dep, tin, tout, ini, id;
 	SEG seg;
 
 	HLD(int _n, vector<int> g[]): 
-		n(_n), anc(n), dep(n+1, -1), pos(n), ini(n), id(n), seg(n) {
+		n(_n), anc(n), dep(n+1), tin(n), tout(n), ini(n), id(n), seg(n) {
 		vector<int> sub(n, 1);
 		function<void(int, int)> pre = [&](int u, int p) {
 			for(int v: g[u]) if(v != p) {
@@ -15,11 +15,10 @@ template<typename SEG, bool EDGE> struct HLD {
 			for(int i=1;i<int(g[u].size());i++) if(sub[g[u][i]] > sub[g[u][0]])
 				swap(g[u][i], g[u][0]);
 		};
-		dep[0] = 0;
 		pre(0, 0);
-		int t = 0, tid = 0;
+		int t = -1, tid = 0;
 		function<void(int,int)> dfs = [&](int u, int p) {
-			pos[u] = t++;
+			tin[u] = ++t;
 			bool fst = true;
 			for(int v: g[u]) if(v != p) {
 				anc[v] = fst ? anc[u] : u;
@@ -28,25 +27,27 @@ template<typename SEG, bool EDGE> struct HLD {
 				dfs(v, u);
 				fst = false;
 			}
+			tout[u] = t;
 		};
+		dep[n] = n;
 		anc[0] = n;
 		dfs(0, 0);
 	}
 
 	template<typename T> void update(int u, T val) {
-		seg.update(pos[u], val);
+		seg.update(tin[u], val);
 	}
 
 	template<typename RES> RES query(int u, int v) {
 		RES res = RES();
 		while(id[u] != id[v]) {
 			if(dep[anc[u]] < dep[anc[v]]) swap(u, v);
-			res = res + seg.query(ini[u], pos[u]);
+			res = res + seg.query(ini[u], tin[u]);
 			u = anc[u];
 		}
-		if(pos[u] > pos[v]) swap(u, v);
-		if(pos[u] + EDGE <= pos[v]) 
-			res = res + seg.query(pos[u] + EDGE, pos[v]);
+		if(tin[u] > tin[v]) swap(u, v);
+		if(tin[u] + EDGE <= tin[v]) 
+			res = res + seg.query(tin[u] + EDGE, tin[v]);
 		return res;
 	}
 };
