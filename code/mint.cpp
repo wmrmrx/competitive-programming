022a065ae5ccc0f52580e94f4f21a46c562c6cf5
wmrmrx <wmrmrx@gmail.com>
2024-02-29 00:00:51 -0300

    Fix

--- code/mint.cpp
+++ code/mint.cpp
@@ -8,10 +8,10 @@ struct mint {
 	mint& operator-=(mint rhs) { x-=rhs.x; if(x<0)x+=MOD; return *this; }
 	mint& operator*=(mint rhs) { x*=rhs.x; x%=MOD; return *this; }
 	mint& operator/=(mint rhs) { *this *= rhs.inv(); return *this; }
-	mint operator+(mint rhs) { mint res=*this; res+=rhs; return res; }
-	mint operator-(mint rhs) { mint res=*this; res-=rhs; return res; }
-	mint operator*(mint rhs) { mint res=*this; res*=rhs; return res; }
-	mint operator/(mint rhs) { mint res=*this; res/=rhs; return res; }
+	mint operator+(mint rhs) { mint res=*this; return res+=rhs; }
+	mint operator-(mint rhs) { mint res=*this; return res-=rhs; }
+	mint operator*(mint rhs) { mint res=*this; return res*=rhs; }
+	mint operator/(mint rhs) { mint res=*this; return res/=rhs; }
 	mint inv() { return this->pow(MOD-2); }
 	mint pow(int e) {
 		mint res(1);
@@ -24,21 +24,21 @@ struct mint {
 	bool operator==(mint rhs) { return x == rhs.x; }
 };
 using Z = mint<998'244'353>;
-
+ 
 struct hash_mint {
 	mint<3000000019> x;
 	mint<3000000109> y;
 	hash_mint(): x(0), y(0) {}
 	hash_mint(int _x, int _y): x(_x), y(_y) {}
 	hash_mint(mint<3000000019> _x, mint<3000000109> _y): x(_x), y(_y) {}
-	hash_mint& operator+=(hash_mint rhs) { return *this += rhs; }
-	hash_mint& operator-=(hash_mint rhs) { return *this -= rhs; }
-	hash_mint& operator*=(hash_mint rhs) { return *this *= rhs; }
-	hash_mint& operator/=(hash_mint rhs) { return *this /= rhs; }
-	hash_mint operator+(hash_mint rhs) { return *this += rhs; }
-	hash_mint operator-(hash_mint rhs) { return *this -= rhs; }
-	hash_mint operator*(hash_mint rhs) { return *this *= rhs; }
-	hash_mint operator/(hash_mint rhs) { return *this /= rhs; }
+	hash_mint& operator+=(hash_mint rhs) { this->x += rhs.x; this->y += rhs.y; return *this; }
+	hash_mint& operator-=(hash_mint rhs) { this->x -= rhs.x; this->y -= rhs.y; return *this; }
+	hash_mint& operator*=(hash_mint rhs) { this->x *= rhs.x; this->y *= rhs.y; return *this; }
+	hash_mint& operator/=(hash_mint rhs) { this->x /= rhs.x; this->y /= rhs.y; return *this; }
+	hash_mint operator+(hash_mint rhs) { hash_mint res = *this; return res += rhs; }
+	hash_mint operator-(hash_mint rhs) { hash_mint res = *this; return res -= rhs; }
+	hash_mint operator*(hash_mint rhs) { hash_mint res = *this; return res *= rhs; }
+	hash_mint operator/(hash_mint rhs) { hash_mint res = *this; return res /= rhs; }
 	hash_mint inv() { return hash_mint(1, 1) / *this; }
 	hash_mint pow(int e) { return hash_mint(x.pow(e), y.pow(e)); }
 	bool operator==(hash_mint rhs) { return x == rhs.x && y == rhs.y; }
