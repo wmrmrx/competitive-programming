c29b849cf2e956c9827ab023980ba917fffe1dd2
Willian Mori <wmrmrx@gmail.com>
2025-03-23 18:40:55 -0300

    Ripoff from jiangly

--- code/mint.cpp
+++ code/mint.cpp
@@ -23,3 +23,47 @@ template <const int64_t MOD> struct mint {
 	friend ostream& operator<<(ostream& o, mint m) { return o << m.x; }
 };
 using Z = mint<998'244'353>;
+
+struct Comb {
+    int n;
+    vector<Z> _fac;
+    vector<Z> _invfac;
+    vector<Z> _inv;
+    
+    Comb() : n{0}, _fac{1}, _invfac{1}, _inv{0} {}
+    Comb(int n) : Comb() {
+        init(n);
+    }
+    void init(int m) {
+        if (m <= n) return;
+        _fac.resize(m + 1);
+        _invfac.resize(m + 1);
+        _inv.resize(m + 1);
+        
+        for (int i = n + 1; i <= m; i++) {
+            _fac[i] = _fac[i - 1] * i;
+        }
+        _invfac[m] = _fac[m].inv();
+        for (int i = m; i > n; i--) {
+            _invfac[i - 1] = _invfac[i] * i;
+            _inv[i] = _invfac[i] * _fac[i - 1];
+        }
+        n = m;
+    }
+    Z fac(int m) {
+        if (m > n) init(2 * m);
+        return _fac[m];
+    }
+    Z invfac(int m) {
+        if (m > n) init(2 * m);
+        return _invfac[m];
+    }
+    Z inv(int m) {
+        if (m > n) init(2 * m);
+        return _inv[m];
+    }
+    Z binom(int n, int m) {
+        if (n < m || m < 0) return 0;
+        return fac(n) * invfac(m) * invfac(n - m);
+    }
+} comb;
